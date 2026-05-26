c9325a21d767565cc2b466f9741340b10771945e
Raveh <raveh@qodeh.com>
2026-05-26 11:49:46 +0300

    Revert "Tutorial: paste-behavior note (trailing-blank doesn't always survive)"
    This reverts commit ecb7406f5e233dbe7e19522162b78c100cfb1525.

--- cmd/smirror/main.go
+++ cmd/smirror/main.go
@@ -40,7 +40,7 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
-var version = "1.0.52-dev"
+var version = "1.0.51-dev"
 
 // Repository coordinates. All runtime references to the GitHub repo (issue
 // URLs, selfupdate API, duplicate search) derive from these two constants.
